c272a7cf41f9484a86a8b7ef60151d1e92343a6d
MonicaRungi <s233813@studenti.polito.it>
2019-05-28 22:12:30 +0200

    chack

--- KMApp/app/src/main/java/com/example/kmapp/DatabaseAccess.java
+++ KMApp/app/src/main/java/com/example/kmapp/DatabaseAccess.java
@@ -81,7 +81,7 @@ public class DatabaseAccess {
     public StringBuffer getUser(int id){
         StringBuffer result = new StringBuffer();
         c=db.rawQuery("select * from UserTable where Id = ?", new String[]{Integer.toString( id )});
-        if(c.moveToFirst()){
+        while(c.moveToNext()){
             String name = c.getString( 1 );
             String text = c.getString( 2 );
             result.append( name ).append( text );
